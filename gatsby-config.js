5b40cd065e57cb3fb9cedcce43ca195f3356543a
Navdeep Singh <navdeep.er@gmail.com>
2020-04-14 16:58:48 +0530

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: ``
+    siteUrl: `https://www.gatsbyjs.org`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
