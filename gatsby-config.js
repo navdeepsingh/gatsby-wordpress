b9356bd14f4dbdf5f1f28569be34f6414fdb2400
Navdeep Singh <navdeep.er@gmail.com>
2020-04-22 16:48:50 +0530

    Update: API protocol

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       options: {
         // I have created a dummy site for us to use with the plugins we discussed
         baseUrl: process.env.GATSBY_API_URL,
-        protocol: "http",
+        protocol: "https",
         hostingWPCOM: false,
         // We will be using some advanced custom fields
         useACF: true,
