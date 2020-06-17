61861b5bee4c63f96609d2163be62fbc8d847290
Navdeep Singh <navdeep.er@gmail.com>
2020-06-17 18:36:09 +0530

    Add: Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,13 +23,52 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Benecol`,
+        short_name: `Benecol`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#00afbc`,
+        theme_color: `#00afbc`,
+        lang: `en`,
+        display: `standalone`,
+        icon: `src/images/android-icon-192x192.png`, // This path is relative to the root of the site.
+        icons: [
+          {
+           src: `/favicons/android-icon-36x36.png`,
+           sizes: `36x36`,
+           type: `image/png`,
+           density: `0.75`
+          },
+          {
+            src: `/favicons/android-icon-48x48.png`,
+            sizes: `48x48`,
+            type: `image/png`,
+            density: `1.0`
+           },
+           {
+            src: `/favicons/android-icon-72x72.png`,
+            sizes: `72x72`,
+            type: `image/png`,
+            density: `1.5`
+           },
+           {
+            src: `/favicons/android-icon-96x96.png`,
+            sizes: `96x96`,
+            type: `image/png`,
+            density: `2.0`
+           },
+           {
+            src: `/favicons/android-icon-144x144.png`,
+            sizes: `144x144`,
+            type: `image/png`,
+            density: `3.0`
+           },
+           {
+            src: `/favicons/android-icon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+            density: `4.0`
+           }
+         ]
       },
     },
     {
